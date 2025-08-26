7c8dd8f2dc2ca56d89918d18fc16be94625aa671
BorisMejia <borisfernandez916@gmail.com>
2025-08-26 15:13:15 -0500

    feat: implement IUserUseCase interface methods in UserUseCase class

--- domain/usecase/src/main/java/co/com/crediya/usecase/user/IUserUseCase.java
+++ domain/usecase/src/main/java/co/com/crediya/usecase/user/IUserUseCase.java
@@ -1,2 +1,10 @@
-package co.com.crediya.usecase.user;public interface IUserUseCase {
+package co.com.crediya.usecase.user;
+
+import co.com.crediya.model.user.User;
+import reactor.core.publisher.Mono;
+
+public interface IUserUseCase {
+
+    Mono<User> createUser(User user);
+    Mono<Boolean> existsByEmail(String email);
 }
--- domain/usecase/src/main/java/co/com/crediya/usecase/user/UserUseCase.java
+++ domain/usecase/src/main/java/co/com/crediya/usecase/user/UserUseCase.java
@@ -1,6 +1,30 @@
 package co.com.crediya.usecase.user;
 
+import co.com.crediya.model.user.User;
+import co.com.crediya.model.user.exception.EmailAlreadyExists;
+import co.com.crediya.model.user.gateways.UserRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
 @RequiredArgsConstructor
-public class UserUseCase {
+public class UserUseCase implements IUserUseCase{
+
+    private final UserRepository userRepository;
+    public Mono<User> createUser(User user) {
+        return userRepository
+                .existsByEmail(user.getEmail())
+                .flatMap(exists ->
+                {
+                    if (exists){
+                        return Mono.error(new EmailAlreadyExists(user.getEmail()));
+                    }
+                    return userRepository.save(user);
+
+                });
+    }
+
+    public Mono<Boolean>  existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+
+    }
 }
