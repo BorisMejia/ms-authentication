d7f446253b4e5c484ce27f47bee300c61ae6dc49
BorisMejia <borisfernandez916@gmail.com>
2025-08-26 15:32:55 -0500

    feat: update MyReactiveRepository and MyReactiveRepositoryAdapter to use UserEntity for user operations

--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/MyReactiveRepositoryAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/MyReactiveRepositoryAdapter.java
@@ -1,23 +1,41 @@
 package co.com.crediya.r2dbc;
 
+import co.com.crediya.model.user.User;
+import co.com.crediya.model.user.gateways.UserRepository;
+import co.com.crediya.r2dbc.entity.UserEntity;
 import co.com.crediya.r2dbc.helper.ReactiveAdapterOperations;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public class MyReactiveRepositoryAdapter extends ReactiveAdapterOperations<
-    Object/* change for domain model */,
-    Object/* change for adapter model */,
+    User/* change for domain model */,
+    UserEntity/* change for adapter model */,
     String,
     MyReactiveRepository
-> {
+> implements UserRepository {
     public MyReactiveRepositoryAdapter(MyReactiveRepository repository, ObjectMapper mapper) {
         /**
          *  Could be use mapper.mapBuilder if your domain model implement builder pattern
          *  super(repository, mapper, d -> mapper.mapBuilder(d,ObjectModel.ObjectModelBuilder.class).build());
          *  Or using mapper.map with the class of the object model
          */
-        super(repository, mapper, d -> mapper.map(d, Object.class/* change for domain model */));
+        super(repository, mapper, d -> mapper.mapBuilder(d,User.UserBuilder.class).build());
     }
 
+    @Override
+    public Mono<User> save(User user) {
+        return null;
+    }
+
+    @Override
+    public Mono<Void> deleteById(String id) {
+        return null;
+    }
+
+    @Override
+    public Mono<Boolean> existsByEmail(String email) {
+        return null;
+    }
 }
