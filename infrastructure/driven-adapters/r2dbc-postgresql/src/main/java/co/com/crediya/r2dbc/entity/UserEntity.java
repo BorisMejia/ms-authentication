794ebee5bfa91740515924fc1814e7a995a8e413
BorisMejia <borisfernandez916@gmail.com>
2025-08-26 15:31:10 -0500

    feat: create UserEntity class for user representation in R2DBC

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/entity/UserEntity.java
@@ -0,0 +1,28 @@
+package co.com.crediya.r2dbc.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Table("usuarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class UserEntity {
+
+    @Id
+    private String id;
+    private String name;
+    private String lastName;
+    private LocalDate birthday;
+    private String address;
+    private String phone;
+    private String email;
+    private BigDecimal baseSalary;
+    private String roleId;
+}
