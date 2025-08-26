12c6296d07ea2a803217cbad68ba2ccaa3bf94bc
BorisMejia <borisfernandez916@gmail.com>
2025-08-26 15:26:58 -0500

    feat: integrate reactive web components and R2DBC for PostgreSQL support

--- applications/app-service/src/main/java/co/com/crediya/config/UseCasesConfig.java
+++ applications/app-service/src/main/java/co/com/crediya/config/UseCasesConfig.java
@@ -1,5 +1,9 @@
 package co.com.crediya.config;
 
+import co.com.crediya.model.user.gateways.UserRepository;
+import co.com.crediya.usecase.user.IUserUseCase;
+import co.com.crediya.usecase.user.UserUseCase;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,4 +15,9 @@ import org.springframework.context.annotation.FilterType;
         },
         useDefaultFilters = false)
 public class UseCasesConfig {
+
+        @Bean
+        public IUserUseCase userUseCase(UserRepository userRepository){
+                return new UserUseCase(userRepository);
+        }
 }
