5bc371bae69943c142260e277d9c1560359b5219
BorisMejia <borisfernandez916@gmail.com>
2025-08-27 16:40:23 -0500

    feat: implement health check endpoint and refactor user routes for improved API structure

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/user/UserHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/user/UserHandler.java
@@ -5,12 +5,16 @@ import co.com.crediya.api.user.dto.request.CreateUserRequestDto;
 import co.com.crediya.usecase.user.IUserUseCase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -20,6 +24,13 @@ public class UserHandler {
     private final IUserUseCase userUseCase;
     private final UserMapperDto userMapperDto;
     private final SpringValidatorAdapter validateAdapter;
+
+
+    public Mono<ServerResponse> health(ServerRequest request) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(Map.of("status","UP","timestamp", Instant.now().toString()));
+    }
 
     public Mono<ServerResponse> createUser(ServerRequest request) {
         return request.bodyToMono(CreateUserRequestDto.class)
