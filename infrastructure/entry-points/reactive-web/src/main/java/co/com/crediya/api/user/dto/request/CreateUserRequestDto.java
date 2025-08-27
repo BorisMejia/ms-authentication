e4e51ec9c97d2ac6f95a2381ece0fea221a96c4a
BorisMejia <borisfernandez916@gmail.com>
2025-08-27 16:38:26 -0500

    feat: add Swagger schema annotations for email and baseSalary fields in CreateUserRequestDto

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/user/dto/request/CreateUserRequestDto.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/user/dto/request/CreateUserRequestDto.java
@@ -1,5 +1,6 @@
 package co.com.crediya.api.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
@@ -11,7 +12,8 @@ public record CreateUserRequestDto(
         LocalDate birthday,
         String     address,
         String     phone,
-        @NotBlank @Email String email,
+        @Schema(example = "example@gmail.com") @NotBlank @Email String email,
+        @Schema(example = "3500000.00", minimum = "0", maximum = "15000000")
         @NotNull @DecimalMin("0") @DecimalMax("15000000") BigDecimal baseSalary,
         String roleId
 ) {
