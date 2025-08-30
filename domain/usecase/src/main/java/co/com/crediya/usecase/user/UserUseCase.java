e414ef6c7856be3f315f313500ce1d94d8a34338
BorisMejia <borisfernandez916@gmail.com>
2025-08-30 11:21:01 -0500

    refactor: replace EmailAlreadyExistsException with ValidationException for improved error handling in UserUseCase

--- domain/usecase/src/main/java/co/com/crediya/usecase/user/UserUseCase.java
+++ domain/usecase/src/main/java/co/com/crediya/usecase/user/UserUseCase.java
@@ -2,6 +2,7 @@ package co.com.crediya.usecase.user;
 
 import co.com.crediya.model.user.User;
 import co.com.crediya.model.user.exception.EmailAlreadyExistsException;
+import co.com.crediya.model.user.exception.ValidationException;
 import co.com.crediya.model.user.gateways.UserRepository;
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
@@ -16,16 +17,16 @@ public class UserUseCase implements IUserUseCase{
                 .flatMap(exists ->
                 {
                     if (exists)
-                        return Mono.error(new EmailAlreadyExistsException(user.getEmail()));
+                        return Mono.error(new ValidationException("Correo electronico no permitido"));
                     if (user.getName() == null || user.getName().isBlank())
-                        return Mono.error(new IllegalArgumentException("Name is required"));
+                        return Mono.error(new ValidationException("Name is required"));
                     if (user.getLastName() == null || user.getLastName().isBlank())
-                        return Mono.error(new IllegalArgumentException("lastName is required"));
+                        return Mono.error(new ValidationException("lastName is required"));
                     if (user.getEmail() == null || user.getEmail().isBlank())
-                        return Mono.error(new IllegalArgumentException("email is required"));
+                        return Mono.error(new ValidationException("email is required"));
                     if (user.getBaseSalary() == null ||
                             user.getBaseSalary() < 0 || user.getBaseSalary() > 15_000_000L)
-                        return Mono.error(new IllegalArgumentException("baseSalary must be between 0 and 15000000"));
+                        return Mono.error(new ValidationException("baseSalary must be between 0 and 15000000"));
                     return userRepository.save(user);
 
                 });
