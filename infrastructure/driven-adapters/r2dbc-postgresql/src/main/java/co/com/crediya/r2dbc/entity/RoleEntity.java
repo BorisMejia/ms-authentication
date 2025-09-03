3fadf2f04a4863c55b0094b2fc39524e12ef81ff
BorisMejia <borisfernandez916@gmail.com>
2025-09-03 15:29:07 -0500

    feat: add RoleEntity class for representing roles in the database

---
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/crediya/r2dbc/entity/RoleEntity.java
@@ -0,0 +1,15 @@
+package co.com.crediya.r2dbc.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Table("roles")
+public class RoleEntity {
+
+    @Id
+    private String id;
+    private String code;
+    private String name;
+}
