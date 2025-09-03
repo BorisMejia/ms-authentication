ab1f80ddba2e95438a4954e9a7e72cf21f6904bb
BorisMejia <borisfernandez916@gmail.com>
2025-09-03 17:24:18 -0500

    feat: implement JWT authentication components and security configuration

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/jwt/components/JwtReactiveAuthenticationManager.java
@@ -0,0 +1,40 @@
+package co.com.crediya.api.jwt.components;
+
+import co.com.crediya.api.jwt.config.JwtAuthenticationToken;
+import co.com.crediya.api.jwt.service.JwtRsaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+public class JwtReactiveAuthenticationManager implements ReactiveAuthenticationManager {
+    private final JwtRsaService jwt;
+
+    public JwtReactiveAuthenticationManager(JwtRsaService jwt) {
+        this.jwt = jwt;
+    }
+
+    @Override
+    public Mono<Authentication> authenticate(Authentication authentication) {
+        if (!(authentication instanceof JwtAuthenticationToken tokenAuth)) return Mono.empty();
+
+        String token = (String) tokenAuth.getCredentials();
+        if (!jwt.validateToken(token)) return Mono.empty();
+
+        String email = jwt.subject(token);
+        String rol   = jwt.role(token);
+        var authorities = List.of(new SimpleGrantedAuthority("ROLE_" + rol));
+
+        var authenticated = new JwtAuthenticationToken(token, email, authorities);
+        return Mono.just(authenticated);
+    }
+
+
+}
+
