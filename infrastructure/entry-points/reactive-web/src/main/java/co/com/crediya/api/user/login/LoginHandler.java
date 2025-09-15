6f00ccd89c75709229f6e7f4eddc72ca6f451323
BorisMejia <borisfernandez916@gmail.com>
2025-09-15 17:25:57 -0500

    feat: add getUserInfoByDocument method to retrieve user information by document

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/user/login/LoginHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/user/login/LoginHandler.java
@@ -5,7 +5,9 @@ import co.com.crediya.api.user.login.support.AuthenticateAndIssueToken;
 import co.com.crediya.api.user.login.support.LoginAssembler;
 import co.com.crediya.api.user.support.ResponseUtils;
 import co.com.crediya.api.user.validations.LoginValidator;
+import co.com.crediya.usecase.user.UserUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -19,6 +21,8 @@ public class LoginHandler {
     private final LoginValidator loginValidator;
     private final LoginAssembler loginAssembler;
     private final AuthenticateAndIssueToken authenticationToken;
+    private final UserUseCase userUseCase;
+
 
     public Mono<ServerResponse> login(ServerRequest req) {
         return req.bodyToMono(LoginRequestDto.class)
@@ -26,6 +30,15 @@ public class LoginHandler {
                 .map(loginAssembler::normalize)
                 .flatMap(authenticationToken::authenticateAndIssueToken)
                 .flatMap(ResponseUtils::okJson);
+    }
+
+    public Mono<ServerResponse> getUserInfoByDocument(ServerRequest request) {
+        String document = request.pathVariable("document");
+        return userUseCase.getUserInfoByDocument(document)
+                .flatMap(userInfo -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .bodyValue(userInfo))
+                .switchIfEmpty(ServerResponse.notFound().build());
     }
 
 }
